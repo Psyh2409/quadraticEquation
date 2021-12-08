403c3258e5c468fedbb92c0ce7610d6852bd3285
Psyh2409 <psyh2409@gmail.com>
2021-12-08 16:42:58 +0200

    add inputter(message) with Scanner and circuler();

--- src/App.java
+++ src/App.java
@@ -1,48 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.Scanner;
 
 public class App {
     
     public static void main(String[] args) {
-            try {
-                qudraticer();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            qudraticer();
+            circuler();
     }
+    
+    public static String inputer(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
+    public static void circuler(){
+        double radius = Double.parseDouble(inputer("Enter radius:"));
+        System.out.println("Area of circle is "+Math.PI*Math.pow(radius, 2));
+        System.out.println("Length of circle is "+2*Math.PI*radius);
+    }
+
     public static void qudraticer() {
-        double a = 0; 
-        double b = 0;
-        double c = 0;
-        try (BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in))) {
-                System.out.println("Enter the first number:");
-                a = Double.parseDouble(br.readLine());
-                System.out.println("Enter the second number:");
-                b = Double.parseDouble(br.readLine());
-                System.out.println("Enter the third number:");
-                c = Double.parseDouble(br.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
+        double a = Double.parseDouble(inputer("Enter the first number:")); 
+        double b = Double.parseDouble(inputer("Enter the second number:"));
+        double c = Double.parseDouble(inputer("Enter the third number:"));
+        double d = Math.pow(b, 2)-4*a*c;
+        double x1 = ((b*-1) + Math.sqrt(d))/2*a;
+        double x2 = ((b*-1) - Math.sqrt(d))/2*a;
+        if(Double.isNaN(x1) && Double.isNaN(x2)){
+            System.out.println(
+                "Equation '"+a+"x^2 + "+b+"x + "+c+" = 0' has no solution.");
+        }else {
+            System.out.println("X1 = " + x1);
+            System.out.println("X2 = " + x2);
         }
-        /**
-     * ax^2 + bx + c = 0;
-     * D = b^2 - 4ac;
-     * x1 = (-b + sqrt(D))/2a;
-     * x2 = (-b - sqrt(D))/2a;
-     * 
-     */
-    double d = Math.pow(b, 2)-4*a*c;
-    double x1 = ((b*-1) + Math.sqrt(d))/2*a;
-    double x2 = ((b*-1) - Math.sqrt(d))/2*a;
-    if(Double.isNaN(x1) && Double.isNaN(x2)){
-        System.out.println(
-            "Equation '"+a+"x^2 + "+b+"x + "+c+" = 0' has no solution.");
-    }else {
-        System.out.println("X1 = " + x1);
-        System.out.println("X2 = " + x2);
-    }
     }
 }
