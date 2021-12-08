e36e2fd9e74241fd4f9b2b67950b211efce8d095
Psyh2409 <psyh2409@gmail.com>
2021-12-08 18:45:50 +0200

    add enum and godAsisstant();

--- src/App.java
+++ src/App.java
@@ -1,16 +1,20 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class App {
-    
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-            qudraticer();
-            circuler();
+        qudraticer();
+        circuler();
+        godAssistant();
+
+        scanner.close();
     }
-    
+
     public static String inputer(String message) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println(message);
-        return scanner.nextLine();
+        String s = scanner.nextLine();
+        return s;
     }
 
     public static void circuler(){
@@ -33,5 +37,46 @@ public class App {
             System.out.println("X1 = " + x1);
             System.out.println("X2 = " + x2);
         }
+    }
+
+    public static void godAssistant() {
+        String day = LocalDate.now().getDayOfWeek().name();
+        System.out.println(day);
+        switch (DaysOfWeek.valueOf(day)) {
+            case MONDAY:
+                System.out.println("God created light and separated the light from the darkness, calling light day and darkness night.");
+                break;
+            case TUESDAY:
+                System.out.println("God created an expanse to separate the waters and called it sky.");
+                break;
+            case WEDNESDAY: 
+                System.out.println("God created the dry ground and gathered the waters, calling the dry ground land, and the gathered waters seas. On day three, God also created vegetation (plants and trees).");
+                break;
+            case THURSDAY:
+                System.out.println("God created the sun, moon, and the stars to give light to the earth and to govern and separate the day and the night. These would also serve as signs to mark seasons, days, and years.");
+                break;
+            case FRIDAY:
+                System.out.println("God created every living creature of the seas and every winged bird, blessing them to multiply and fill the waters and the sky with life.");
+                break;
+            case SATURDAY:
+                System.out.println("God created the animals to fill the earth. On day six, God also created man and woman (Adam and Eve) in his own image to commune with him. He blessed them and gave them every creature and the whole earth to rule over, care for, and cultivate.");
+                break;
+            case SUNDAY:
+                System.out.println("God had finished his work of creation and so he rested on the seventh day, blessing it and making it holy.");
+                break;
+                default:
+                System.out.println("What kind of god are you if seven days are not enough for you to create the world?! Loooser!");
+                break;
+        } 
+    }
+
+    public enum DaysOfWeek{
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
     }
 }
